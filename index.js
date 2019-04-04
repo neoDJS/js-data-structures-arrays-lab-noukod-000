a65bb11474dbb1ef4836bb4d247b2b123c20299b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 18:43:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,13 +18,17 @@ function destructivelyRemoveFirstDriver(){
 }
 
 function appendDriver(name){
-  return [...drivers, name]
+  return [...drivers, name];
 }
 
 function prependDriver(name){
-  return [name, ...drivers]
+  return [name, ...drivers];
 }
 
 function removeLastDriver(){
-  return drivers.slice(0, drivers.length-1)
+  return drivers.slice(0, drivers.length-1);
+}
+
+function removeFirstDriver(){
+  return drivers.slice(1);
 }
