74fe58861c3bc871bf4ac460f9f1e08292cd4c34
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 18:38:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
